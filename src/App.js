e3c781e05e1853ddfb56596f3eff332b0e0a2d5c
Roshan <nkroshankumar@gmail.com>
2022-10-30 22:12:51 +0530

    added movement for players

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Physics } from "@react-three/cannon"
 import { Sky } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
+import Cubes from "./components/Cubes"
+import FPV from "./components/FPV"
 import Ground from "./components/Ground"
 import Player from "./components/Player"
 
@@ -10,11 +12,14 @@ function App() {
             <Canvas>
                 <Sky sunPosition={[100, 100, 10]} />
                 <ambientLight intensity={0.5} />
+                <FPV />
                 <Physics>
                     <Player />
+                    <Cubes />
                     <Ground />
                 </Physics>
             </Canvas>
+            <h1 className="center">+</h1>
         </>
     )
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -5,6 +5,7 @@ import { Vector3 } from "three"
 import useKeyboard from "../hooks/useKeyboard"
 
 const JUMP_FORCE = 5
+const SPEED = 5
 
 const Player = () => {
     const { camera } = useThree()
@@ -14,7 +15,8 @@ const Player = () => {
         position: [0, 1, 0],
     }))
 
-    const actions = useKeyboard()
+    const { jump, moveForward, moveBackward, moveLeft, moveRight } =
+        useKeyboard()
 
     const pos = useRef([0, 0, 0])
     const velocity = useRef([0, 0, 0])
@@ -32,7 +34,27 @@ const Player = () => {
             new Vector3(pos.current[0], pos.current[1], pos.current[2])
         )
 
-        if (actions.jump && Math.abs(velocity.current[1] < 0.0001)) {
+        const direction = new Vector3()
+        const frontVector = new Vector3(
+            0,
+            0,
+            (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+        )
+        const sideVector = new Vector3(
+            (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+            0,
+            0
+        )
+
+        direction
+            .subVectors(frontVector, sideVector)
+            .normalize()
+            .multiplyScalar(SPEED)
+            .applyEuler(camera.rotation)
+
+        api.velocity.set(direction.x, velocity.current[1], direction.z)
+
+        if (jump && Math.abs(velocity.current[1] < 0.0001)) {
             api.velocity.set(
                 velocity.current[0],
                 JUMP_FORCE,
